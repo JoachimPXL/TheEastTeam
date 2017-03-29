5be89f6c2877109c4513d147110f5cc1be4dbece
CollaerWietse <wietse.collaer@student.pxl.be>
2017-03-29 15:16:29 +0200

    Messenger Front-End toegevoegd

---
+++ Messenger Front-end/src/login.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Wiets
+ * Date: 11/03/2017
+ * Time: 10:14
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="../assets/style/layout.css">
+</head>
+<body>
+<div class="loginImage">
+    <div class="loginBar">
+        <div class="content">
+            <h1>Basic Security Messenger</h1>
+            <p>Meld u aan met uw gebruikersnaam en wachtwoord</p>
+            <form action="home.php">
+                <div class="inputField">
+                    <input type="text" placeholder="Gebruikersnaam">
+                </div>
+                <div class="inputField">
+                    <input type="password" placeholder="Wachtwoord">
+                </div>
+                <input type="submit" value="AANMELDEN" class="button">
+            </form>
+        </div>
+
+    </div>
+</div>
+</body>
+</html>
